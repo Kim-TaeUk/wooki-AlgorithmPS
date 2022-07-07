4fcdddfdb5ceb0591cab7ce5dea777805bfabf60
Kim-TaeUk <xodnr0130@naver.com>
2022-07-08 03:12:22 +0900

    3. 조건문(if 분기문, 다중 if문)

---
+++ inflearn_lecture/section01/basic_syntax_03.py
@@ -0,0 +1,37 @@
+# 3. 조건문(if 분기문, 다중 if문)
+x = 15
+
+if x == 15:
+    print("Lucky")
+if x != 7:
+    print("Unlucky")
+
+# 아래 3개 case가 모두 같은 의미
+if x > 0:
+    if x < 20:
+        print("20보다 작은 자연수이다")
+
+if x > 0 and x < 20:
+    print("20보다 작은 자연수이다")
+
+if 0 < x < 20:
+    print("20보다 작은 자연수이다")
+
+# if - else (분기)
+if x > 0:
+    print("양수이다")
+else:
+    print("음수이다")
+
+# if - elif - else (다중 분기)
+x = 93
+if x >= 90:
+    print('A')
+elif x >= 80:
+    print('B')
+elif x >= 70:
+    print('C')
+elif x >= 60:
+    print('D')
+else:
+    print('F')
