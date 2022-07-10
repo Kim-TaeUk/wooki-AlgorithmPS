6bc672d3d6e262169160a469cf787625ccb0c24f
Kim-TaeUk <xodnr0130@naver.com>
2022-07-11 02:49:37 +0900

    2주차 3일차 G - 2 코드 수정

--- AlgorithmTA/G2김태욱.py
+++ AlgorithmTA/G2김태욱.py
@@ -1,18 +1,14 @@
 # G - 2
 n = int(input())
 
-ugly = [0] * n  # 못생긴 수를 담기 위한 테이블 (1차원 DP 테이블)
+ugly = [0] * n
 ugly[0] = 1  # 첫 번째 못생긴 수는 1
 
-# 2배, 3배, 5배를 위한 인덱스
-i2 = i3 = i5 = 0
-# 처음에 곱셈 값을 초기화
-next2, next3, next5 = 2, 3, 5
+i2 = i3 = i5 = 0  # 2배, 3배, 5배를 위한 인덱스
+next2, next3, next5 = 2, 3, 5  # 처음에 곱셈 값을 초기화
 
-# 1부터 n까지의 못생긴 수들을 찾기
 for i in range(1, n):
-    # 가능한 곱셈 결과 중에서 가장 작은 수를 선택
-    ugly[i] = min(next2, next3, next5)
+    ugly[i] = min(next2, next3, next5)  # 가능한 곱셈 결과 중에서 가장 작은 수를 선택
     # 인덱스에 따라서 곱셈 결과를 증가
     if ugly[i] == next2:
         i2 += 1
