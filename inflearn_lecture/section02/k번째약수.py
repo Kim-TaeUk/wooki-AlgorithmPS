d489a0a2617cfdc1e02ad4ca59db673a5b9c0ab8
Kim-TaeUk <xodnr0130@naver.com>
2022-12-04 01:03:01 +0900

    k번째 약수

--- inflearn_lecture/section02/k번째약수.py
+++ inflearn_lecture/section02/k번째약수.py
@@ -1,11 +1,11 @@
 N, K = map(int, input().split())
-list = []
+list = []  # 약수를 저장할 list
 
-for i in range(1, N + 1):
-    if N % i == 0:
-        list.append(i)
+for i in range(1, N + 1):  # 1부터 N까지 1씩 증가하면서
+    if N % i == 0:  # N을 나눴을 때 나머지가 0이면 약수니까
+        list.append(i)  # list에 추가
 
-if len(list) < K:
-    print(-1)
+if len(list) < K:  # 약수의 개수가 K보다 작으면
+    print(-1)  # -1 출력
 else:
-    print(list[K - 1])
+    print(list[K - 1])  # K번째 작은 수 출력
