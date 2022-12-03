2d4a99a1e164501e8e23fea6fe478263d314c658
Kim-TaeUk <xodnr0130@naver.com>
2022-12-04 01:03:09 +0900

    k번째 약수 - 해설

--- inflearn_lecture/section02/k번째약수_sol.py
+++ inflearn_lecture/section02/k번째약수_sol.py
@@ -1,14 +1,14 @@
 N, K = map(int, input().split())
 list = []
 
-cnt = 0
+cnt = 0  # cnt 도입해서
 for i in range(1, N + 1):
     if N % i == 0:
-        cnt += 1
-    if cnt == K:
-        print(i)
-        break
-else:
+        cnt += 1  # 약수면 cnt 증가시켜 주면서
+    if cnt == K:  # K번째이면
+        print(i)  # 바로 출력
+        break  # break해서 원하는 거 출력하면 굳이 끝까지 안찾아도 됨
+else:  # 예외처리는 for - else로 해버리고
     print(-1)
 
 # list에 일일이 약수를 넣고 K번째로 찾아가서 출력하는 것보다
