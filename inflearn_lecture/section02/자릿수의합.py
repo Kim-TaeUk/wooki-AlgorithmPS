2c330cf589f83dfc0696039b73c7cab6798dec57
Kim-TaeUk <xodnr0130@naver.com>
2022-12-04 07:34:15 +0900

    자릿수의 합

---
+++ inflearn_lecture/section02/자릿수의합.py
@@ -0,0 +1,20 @@
+N = int(input())
+list = list(map(int, input().split()))
+
+
+def digit_sum(x):
+    sum = 0
+    while x > 0:
+        sum += x % 10
+        x = x // 10
+    return sum
+
+
+max = float('-inf')
+for x in list:
+    sum = digit_sum(x)
+    if sum > max:
+        max = sum
+        res = x
+
+print(x)
