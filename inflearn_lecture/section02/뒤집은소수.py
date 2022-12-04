7acd98c23574a548bc1fec3b290118b18e77fbd0
Kim-TaeUk <xodnr0130@naver.com>
2022-12-05 01:16:35 +0900

    뒤집은 소수 - 수정

--- inflearn_lecture/section02/뒤집은소수.py
+++ inflearn_lecture/section02/뒤집은소수.py
@@ -17,6 +17,8 @@ def reverse(x):
 
 
 def isPrime(k):
+    if k == 1:
+        return False
     flag = 0  # flag 도입
     for i in range(2, k):
         if k % i == 0:  # 나누어 떨어지는게 있으면 flag 1로
@@ -41,3 +43,13 @@ for i in rvs:
 
 for i in range(len(res)):  # 출력
     print(res[i], end=' ')
+
+# reverse()에서 list로 하나씩 분리한 다음 다시 꺼내서 10의 거듭제곱을 곱해줬는데
+# sol 방식이 훨씬 깔끔하고 빠름.. -> reverse하는건 자주 나올 수 있으니 저대로 풀자 앞으로는
+
+# isPrime()에서 1일 경우 예외처리 해야한다 -> 1은 소수가 아니니까!!
+# isPrime()에서 for - else 구문 사용하면 flag 도입할 필요가 없음 -> 배웠으면 사용하자!
+# isPrime()에서 절반까지만 나눠보면 된다 -> 실행속도 줄이기..!
+
+# 결과 출력할 때 굳이 list에 다시 넣지 말고, isPrime의 return이 boolean이니까
+# 조건문 만족하면 바로바로 뽑으면 된다 -> 2번 일 하지 말자
