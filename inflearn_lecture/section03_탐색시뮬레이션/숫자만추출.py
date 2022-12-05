15fdd2c44ded6017a216d42e7c6d838616484065
Kim-TaeUk <xodnr0130@naver.com>
2022-12-05 16:14:43 +0900

    숫자만 추출

---
+++ inflearn_lecture/section03_탐색시뮬레이션/숫자만추출.py
@@ -0,0 +1,16 @@
+msg = input()
+num_list = []
+
+for x in msg:
+    if x.isdigit():  # isdigit()으로 숫자만
+        num_list.append(x)  # num_list에 append
+
+num_list = "".join(num_list)  # 문자열로 만들고
+num = int(num_list)  # int로 casting
+
+cnt = 0
+for i in range(1, num + 1):
+    if num % i == 0:
+        cnt += 1
+
+print(cnt)
