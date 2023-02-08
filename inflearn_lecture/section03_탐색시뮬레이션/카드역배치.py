56b576bcc63e6856068e2d4d9a352cc0f018c893
KimTaeUk <xodnr0130@naver.com>
2023-02-09 05:50:15 +0900

    카드 역배치 - 수정

--- inflearn_lecture/section03_탐색시뮬레이션/카드역배치.py
+++ inflearn_lecture/section03_탐색시뮬레이션/카드역배치.py
@@ -5,9 +5,9 @@ for i in range(21):
 for i in range(10):
     a, b = map(int, input().split())
 
-    temp = list[a:b + 1]
-    temp.reverse()
-    list[a:b + 1] = temp
+    temp = list[a:b + 1]  # 구간을 temp로 따로 떼내고
+    temp.reverse()  # reverse로 뒤집고
+    list[a:b + 1] = temp  # 뒤집은 걸 떼어낸 데에 붙임
 
 for i in range(1, len(list)):
     print(list[i], end=' ')
