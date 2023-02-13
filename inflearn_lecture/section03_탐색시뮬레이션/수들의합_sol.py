faf600b18195127af0de474c86249e66626c57e4
KimTaeUk <xodnr0130@naver.com>
2023-02-13 10:19:12 +0900

    수들의 합 - 해설

--- inflearn_lecture/section03_탐색시뮬레이션/수들의합_sol.py
+++ inflearn_lecture/section03_탐색시뮬레이션/수들의합_sol.py
@@ -22,3 +22,11 @@ while True:
         lt += 1
 
 print(cnt)
+
+# sum은 lt ~ rt-1의 부분합 -> list[0]으로 초기화시켜놓음 일단
+# sum < M이면 rt를 한 칸 뒤로 넘기고, rt까지 sum에 추가
+# 근데 sum < M이어도 rt가 N보다 같거나 커버리면 while문 탈출하게
+
+# sum = M이면 cnt +1, sum에서 lt가 가리키는 값 빼고 lt는 한 칸 뒤로 넘기고
+
+# sum > M이면 sum에서 lt가 가리키는 값 빼고 lt는 한 칸 뒤로 넘기고
