3fb2156e3872e2d34247b328df988890bdd11088
KimTaeUk <xodnr0130@naver.com>
2023-02-13 10:23:48 +0900

    수들의 합 - 수정

--- inflearn_lecture/section03_탐색시뮬레이션/수들의합.py
+++ inflearn_lecture/section03_탐색시뮬레이션/수들의합.py
@@ -19,3 +19,9 @@ for i in range(N):
         sum += list[temp]
 
 print(cnt)
+
+# 바깥 for문에서 i를 인덱스로, sum은 list[i]로, temp = i로
+# 안에서 루프 돌리는데, sum < M이면 그냥 계속 도는게 기본 가정
+# sum = M이면 cnt +1, while문 break
+# sum > M이면 break
+# sum < M이면 temp를 +1하고 temp가 가리키는 값까지 sum에 더해주면서 계속 돌게
