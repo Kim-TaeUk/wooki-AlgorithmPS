34a9eb2d09db3fbff814cd358b648fb98f003203
KimTaeUk <xodnr0130@naver.com>
2023-04-13 06:18:51 +0900

    뮤직 비디오(결정 알고리즘) - 해설

---
+++ inflearn_lecture/section04_이분탐색(결정알고리즘)&그리디알고리즘/뮤직비디오(결정알고리즘)_sol.py
@@ -0,0 +1,29 @@
+def count(capacity):
+    cnt = 1
+    sum = 0
+    for x in n_list:
+        if sum + x > capacity:
+            cnt += 1
+            sum = x
+        else:
+            sum += x
+
+    return cnt
+
+
+N, M = map(int, input().split())
+n_list = list(map(int, input().split()))
+
+res = 0
+left = 1
+right = sum(n_list)
+
+while left <= right:
+    mid = (left + right) // 2
+    if count(mid) <= M:
+        res = mid
+        right = mid - 1
+    else:
+        left = mid + 1
+
+print(res)
