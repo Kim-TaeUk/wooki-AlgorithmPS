e650bf873c34b7b39768594b1ec53f6a3a5f0d90
KimTaeUk <xodnr0130@naver.com>
2023-04-17 17:25:02 +0900

    가장 큰 수 - 해설

--- inflearn_lecture/section05_자료구조(stack,queue,hash,heap)/가장큰수_sol.py
+++ inflearn_lecture/section05_자료구조(stack,queue,hash,heap)/가장큰수_sol.py
@@ -4,12 +4,12 @@ num = list(map(int, str(num)))
 stack = []
 
 for x in num:
-    while stack and m > 0 and stack[-1] < x:
+    while stack and m > 0 and stack[-1] < x:  # stack이 비어있지 않을 때만 돌아야 하는 것을 stack으로
         stack.pop()
         m -= 1
     stack.append(x)
 
-if m != 0:
+if m != 0:  # 뒤에서 pop해야할 것이 m개 남았으니까 그냥 slicing 처리 해버리는 아이디어
     stack = stack[:-m]
 
 res = "".join(map(str, stack))
