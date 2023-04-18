8f3807d24412b3325aab8e76cd743ad4a8f9fc44
KimTaeUk <xodnr0130@naver.com>
2023-04-19 06:35:10 +0900

    공주 구하기 queue 사용 O

---
+++ inflearn_lecture/section05_자료구조(stack,queue,hash,heap)/공주구하기2.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+N, K = map(int, input().split())
+
+queue = deque([x for x in range(1, N + 1)])
+
+acc = 1
+while len(queue) != 1:
+    if acc % K == 0:
+        queue.popleft()
+        acc += 1
+    else:
+        queue.append(queue.popleft())
+        acc += 1
+
+print(queue.popleft())
