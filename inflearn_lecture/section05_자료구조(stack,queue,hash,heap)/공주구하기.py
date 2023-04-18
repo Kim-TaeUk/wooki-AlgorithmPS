29e497602653007a06b052364d31f0df6938b40c
KimTaeUk <xodnr0130@naver.com>
2023-04-19 06:13:38 +0900

    공주 구하기

---
+++ inflearn_lecture/section05_자료구조(stack,queue,hash,heap)/공주구하기.py
@@ -0,0 +1,19 @@
+N, K = map(int, input().split())
+
+prince = [x for x in range(1, N + 1)]
+tmp = [x for x in range(1, N + 1)]
+
+acc = 1
+while len(tmp) != 1:
+    for i in range(len(prince)):
+        if prince[i] == 0:
+            pass
+        else:
+            if acc % K == 0:
+                tmp.remove(prince[i])
+                prince[i] = 0
+                acc += 1
+            else:
+                acc += 1
+
+print(tmp.pop())
