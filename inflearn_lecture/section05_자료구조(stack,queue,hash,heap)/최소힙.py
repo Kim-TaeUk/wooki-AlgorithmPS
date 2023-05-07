aa189288ad8efab0acfd45e2440de3f90c880965
KimTaeUk <xodnr0130@naver.com>
2023-05-07 22:35:51 +0900

    최소 힙

---
+++ inflearn_lecture/section05_자료구조(stack,queue,hash,heap)/최소힙.py
@@ -0,0 +1,14 @@
+import heapq as hq
+
+lst = []
+while True:
+    n = int(input())
+    if n == -1:
+        break
+    if n == 0:
+        if len(lst) == 0:
+            print(-1)
+        else:
+            print(hq.heappop(lst))
+    else:
+        hq.heappush(lst, n)
