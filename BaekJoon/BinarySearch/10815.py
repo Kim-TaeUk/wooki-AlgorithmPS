5cae448cc57dc61807a4fca551f3167849ac6cea
Kim-TaeUk <xodnr0130@naver.com>
2023-09-06 01:17:44 +0900

    숫자 카드 - 시간 초과(이분 탐색 + list 대신 dict -> 시간 초과)

--- BaekJoon/BinarySearch/10815.py
+++ BaekJoon/BinarySearch/10815.py
@@ -3,10 +3,34 @@ N_list = list(map(int, input().split()))
 M = int(input())
 M_list = list(map(int, input().split()))
 
-for x in M_list:
-    if x not in N_list:
-        print(0, end=" ")
-    else:
-        print(1, end=" ")
+M_dict = dict.fromkeys(M_list)
 
-# 모든 원소 탐색 -> 시간 초과
+N_list.sort()
+M_list.sort()
+
+for i in range(len(M_list)):
+    left = 0
+    right = len(N_list) - 1
+
+    while True:
+        mid = (left + right) // 2
+
+        if left <= right:
+            if N_list[mid] > M_list[i]:
+                right = mid - 1
+            elif N_list[mid] < M_list[i]:
+                left = mid + 1
+            elif N_list[mid] == M_list[i]:
+                # print(1, end=" ")
+                M_dict[M_list[i]] = 1
+                N_list.remove(N_list[mid])
+                break
+        else:
+            # print(0, end=" ")
+            M_dict[M_list[i]] = 0
+            break
+
+for ans in M_dict:
+    print(M_dict[ans], end=" ")
+
+# 이분 탐색 + list 대신 dict -> 시간 초과
