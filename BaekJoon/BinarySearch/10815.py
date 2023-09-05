fff97212246fb2a02728bdda436f180a99758186
Kim-TaeUk <xodnr0130@naver.com>
2023-09-06 01:20:30 +0900

    숫자 카드 - 시간 초과(이분 탐색 / dict 사용 X, M_list 정렬 X)

--- BaekJoon/BinarySearch/10815.py
+++ BaekJoon/BinarySearch/10815.py
@@ -3,34 +3,25 @@ N_list = list(map(int, input().split()))
 M = int(input())
 M_list = list(map(int, input().split()))
 
-M_dict = dict.fromkeys(M_list)
-
 N_list.sort()
-M_list.sort()
 
 for i in range(len(M_list)):
     left = 0
     right = len(N_list) - 1
 
-    while True:
+    while left <= right:
         mid = (left + right) // 2
 
-        if left <= right:
-            if N_list[mid] > M_list[i]:
-                right = mid - 1
-            elif N_list[mid] < M_list[i]:
-                left = mid + 1
-            elif N_list[mid] == M_list[i]:
-                # print(1, end=" ")
-                M_dict[M_list[i]] = 1
-                N_list.remove(N_list[mid])
-                break
-        else:
-            # print(0, end=" ")
-            M_dict[M_list[i]] = 0
+        if N_list[mid] > M_list[i]:
+            right = mid - 1
+        elif N_list[mid] < M_list[i]:
+            left = mid + 1
+        elif N_list[mid] == M_list[i]:
+            print(1, end=" ")
+            N_list.remove(N_list[mid])
             break
 
-for ans in M_dict:
-    print(M_dict[ans], end=" ")
+    if left > right:
+        print(0, end=" ")
 
-# 이분 탐색 + list 대신 dict -> 시간 초과
+# 이분 탐색 / dict 사용 X, M_list 정렬 X -> 시간 초과
