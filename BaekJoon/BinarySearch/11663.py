2c02fed924304010474b378cb9fdb38bfff3c5f2
Kim-TaeUk <xodnr0130@naver.com>
2023-09-07 04:15:47 +0900

    선분 위의 점 - 시간 초과(이분 탐색)

--- BaekJoon/BinarySearch/11663.py
+++ BaekJoon/BinarySearch/11663.py
@@ -2,11 +2,43 @@ N, M = map(int, input().split())
 N_list = list(map(int, input().split()))
 M_list = [list(map(int, input().split())) for _ in range(M)]
 
-for i in range(M):
-    cnt = 0
-    for x in N_list:
-        if M_list[i][0] <= x <= M_list[i][1]:
-            cnt += 1
-    print(cnt)
+N_list.sort()
 
-# 이중 for문 -> 시간 초과
+for i in range(M):
+    left1 = 0
+    right1 = N - 1
+    left2 = 0
+    right2 = N - 1
+    x1 = -1
+    x2 = -1
+
+    while left1 <= right1:
+        mid1 = (left1 + right1) // 2
+
+        if M_list[i][0] < N_list[mid1]:
+            right1 = mid1 - 1
+        elif M_list[i][0] == N_list[mid1]:
+            x1 = mid1
+            break
+        elif M_list[i][0] > N_list[mid1]:
+            left1 = mid1 + 1
+
+    while left2 <= right2:
+        mid2 = (left2 + right2) // 2
+
+        if M_list[i][1] < N_list[mid2]:
+            right2 = mid2 - 1
+        elif M_list[i][1] == N_list[mid2]:
+            x2 = mid2
+            break
+        elif M_list[i][1] > N_list[mid2]:
+            left2 = mid2 + 1
+
+    if x1 == -1:
+        x1 = left1
+
+    if x2 == -1:
+        x2 = right2
+    print(x2 - x1 + 1)
+
+# 이분 탐색 -> 시간 초과
