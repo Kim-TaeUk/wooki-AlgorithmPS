d6da18cac3d208bcb1579785d4854132143e449c
Kim-TaeUk <xodnr0130@naver.com>
2023-09-12 18:42:22 +0900

    알파벳 찾기 - 다른 풀이들 추가

--- BaekJoon/Implementation/10809.py
+++ BaekJoon/Implementation/10809.py
@@ -10,17 +10,20 @@ for i in range(len(word)):
 for x in alphabet:
     print(x, end=" ")
 
-# 풀이 1 - index() 이용
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+# # 풀이 1 - index() 이용
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+#
+# for x in alphabet:
+#     if x in word:
+#         print(word.index(x), end=" ")
+#     else:
+#         print(-1, end=" ")
+#
+# # 풀이 2 - ASCII + find() 이용
+# alphabet = list(range(97, 123))
+#
+# for x in alphabet:
+#     print(word.find(chr(x)), end=" ")
 
-for x in alphabet:
-    if x in word:
-        print(word.index(x), end=" ")
-    else:
-        print(-1, end=" ")
-
-# 풀이 2 - ASCII + find() 이용
-alphabet = list(range(97, 123))
-
-for x in alphabet:
-    print(word.find(chr(x)), end=" ")
+# input 길이가 100이 넘진 않긴 하지만, word 순회보다는 alphabet 순회가 나은거 같다
+# 쉬운 문제인데 내 풀이보다는 다른 풀이들이 더 나아보임.. 풀이 1이 제일 나은 듯
