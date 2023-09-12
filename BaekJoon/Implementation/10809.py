bdd239146611ccb602e48652251684d141249aed
Kim-TaeUk <xodnr0130@naver.com>
2023-09-12 18:23:34 +0900

    알파벳 찾기 - 다른 풀이들 추가

--- BaekJoon/Implementation/10809.py
+++ BaekJoon/Implementation/10809.py
@@ -1,5 +1,6 @@
 word = input()
 
+# 내 풀이 - ASCII 이용
 alphabet = [-1] * 26
 
 for i in range(len(word)):
@@ -8,3 +9,18 @@ for i in range(len(word)):
 
 for x in alphabet:
     print(x, end=" ")
+
+# 풀이 1 - index() 이용
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+for x in alphabet:
+    if x in word:
+        print(word.index(x), end=" ")
+    else:
+        print(-1, end=" ")
+
+# 풀이 2 - ASCII + find() 이용
+alphabet = list(range(97, 123))
+
+for x in alphabet:
+    print(word.find(chr(x)), end=" ")
