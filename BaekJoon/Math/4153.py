36d1dc3f431f164db2fb0a73bef39b118518dcd3
Kim-TaeUk <xodnr0130@naver.com>
2023-09-14 18:12:16 +0900

    직각 삼각형

---
+++ BaekJoon/Math/4153.py
@@ -0,0 +1,10 @@
+while True:
+    edges = list(map(int, input().split()))
+    if sum(edges) == 0:
+        break
+    else:
+        edges.sort()
+        if edges[0] ** 2 + edges[1] ** 2 == edges[2] ** 2:
+            print('right')
+        else:
+            print('wrong')
