90037680fcc64ae85a18792c6d2fa13d2f197b64
Kim-TaeUk <xodnr0130@naver.com>
2023-09-20 16:03:12 +0900

    수 정렬하기3

--- BaekJoon/Implementation/10989.py
+++ BaekJoon/Implementation/10989.py
@@ -1,11 +1,13 @@
-N = int(input())
+import sys
+
+N = int(sys.stdin.readline().strip())
 count = [0] * 10001
 for _ in range(N):
-    count[int(input())] += 1
+    count[int(sys.stdin.readline().strip())] += 1
 
 for i in range(10001):
     for _ in range(count[i]):
         print(i)
 
 # numbers에 들어가는 수가 10,000보다 작다는 것을 활용
-# 시간 초과
+# 시간 초과 -> sys로 해결
