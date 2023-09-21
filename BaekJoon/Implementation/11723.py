55b3c047295265aa67b466a9203db120366f514d
Kim-TaeUk <xodnr0130@naver.com>
2023-09-21 18:25:42 +0900

    집합

--- BaekJoon/Implementation/11723.py
+++ BaekJoon/Implementation/11723.py
@@ -21,8 +21,10 @@ for _ in range(N):
         else:
             myset.add(order[1])
     elif order[0] == 'all':
-        myset = set([str(x + 1) for x in range(20)])
+        myset = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18',
+                 '19', '20'}
     elif order[0] == 'empty':
         myset.clear()
 
-# 시간 초과
+# 시간 초과 - 'all' 조건에서 집합 직접 할당으로 해결
+# 비트 마스킹으로도 해결 가능하다고 함..
