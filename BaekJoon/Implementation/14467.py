476ee318ddc0c2255b9de7996952ef1d1cdfb072
Kim-TaeUk <xodnr0130@naver.com>
2023-09-22 16:22:20 +0900

    소가 길을 건너는 이유 1

--- BaekJoon/Implementation/14467.py
+++ BaekJoon/Implementation/14467.py
@@ -3,15 +3,18 @@ import sys
 N = int(sys.stdin.readline().rstrip())
 
 cnt = 0
-cows = [[x + 1, -1] for x in range(10)]
+cows = [-1] * 11
 
 for _ in range(N):
     number, location = map(int, sys.stdin.readline().split())
 
-    if cows[number - 1][1] != location and cows[number - 1][1] != -1:
+    if cows[number] == -1:
+        cows[number] = location
+    elif cows[number] != location:
+        cows[number] = location
         cnt += 1
-        cows[number - 1][1] = location
-    else:
-        cows[number - 1][1] = location
 
 print(cnt)
+
+# 2차원 배열 사용X -> 개선
+# 1차원 배열 사용 -> 메모리 효율적
