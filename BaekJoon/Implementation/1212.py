69e745fa93eff5296e874cc55548cb479ed88c23
Kim-TaeUk <xodnr0130@naver.com>
2023-09-22 16:59:24 +0900

    8진수 2진수

--- BaekJoon/Implementation/1212.py
+++ BaekJoon/Implementation/1212.py
@@ -1,24 +1,7 @@
 import sys
 
+Octal = sys.stdin.readline().rstrip()
 
-def tobin(target):
-    if target == 0:
-        return
-    else:
-        tobin(target // 2)
-        print(target % 2, end="")
+print(bin(int(Octal, 8))[2:])
 
-
-Octal = list(map(int, sys.stdin.readline().rstrip()))
-Decimal = 0
-x = 1
-
-for i in range(len(Octal) - 1, -1, -1):
-    Decimal += x * Octal[i]
-    x *= 8
-
-tobin(Decimal)
-
-# tobin(): O(logN)
-# for loop: O(N)
-# 시간복잡도 O(N)이라 괜찮을 줄 알았는데 시간 초과..
+# 내장함수 이용해서 해결
