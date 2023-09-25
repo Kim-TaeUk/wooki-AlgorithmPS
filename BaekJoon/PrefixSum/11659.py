fb9dcee21baee202918dfddbb9563461d218c668
Kim-TaeUk <xodnr0130@naver.com>
2023-09-26 01:37:03 +0900

    구간 합 구하기 4

--- BaekJoon/PrefixSum/11659.py
+++ BaekJoon/PrefixSum/11659.py
@@ -3,9 +3,16 @@ import sys
 N, M = map(int, sys.stdin.readline().split())
 numbers = list(map(int, sys.stdin.readline().split()))
 
-prefixsum = [0] * (N + 1)
-for i in range(N):
-    prefixsum[i + 1] = prefixsum[i] + numbers[i]
+# prefixsum = [0] * (N + 1)
+# for i in range(N):
+#     prefixsum[i + 1] = prefixsum[i] + numbers[i]
+
+# 아래 코드가 더 직관적이라 나은 듯
+prefixsum = [0]
+tmp = 0
+for x in numbers:
+    tmp += x
+    prefixsum.append(tmp)
 
 for _ in range(M):
     left, right = map(int, sys.stdin.readline().split())
