49a2b79715c35f400bafdc165c4d5af9b5fa4f76
Kim-TaeUk <xodnr0130@naver.com>
2023-09-25 23:37:16 +0900

    별 찍기 - 8

--- BaekJoon/Implementation/2445.py
+++ BaekJoon/Implementation/2445.py
@@ -5,8 +5,4 @@ N = int(sys.stdin.readline().rstrip())
 for times in range(1, N + 1):
     print("*" * times + " " * 2 * (N - times) + "*" * times)
 for times in range(1, N):
-<<<<<<< HEAD
     print("*" * (N - times) + " " * 2 * times + "*" * (N - times))
-=======
-    print("*"* (N - times) + " " * 2 * times + "*" * (N - times))
->>>>>>> origin/master
