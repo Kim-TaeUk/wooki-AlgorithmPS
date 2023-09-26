a6fc132abf14bfe06da4b566fb44150c7ea53d5a
Kim-TaeUk <xodnr0130@naver.com>
2023-09-27 04:30:31 +0900

    구간 합 구하기 5

---
+++ BaekJoon/PrefixSum/11660.py
@@ -0,0 +1,20 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+numbers = [list(map(int, input().split())) for _ in range(N)]
+prefixsum = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
+
+for i in range(N):
+    for j in range(N):
+        prefixsum[i + 1][j + 1] = (prefixsum[i + 1][j]
+                                   + prefixsum[i][j + 1]
+                                   - prefixsum[i][j]
+                                   + numbers[i][j])
+for _ in range(M):
+    x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
+    regionsum = (prefixsum[x2][y2]
+                 - prefixsum[x1 - 1][y2]
+                 - prefixsum[x2][y1 - 1]
+                 + prefixsum[x1 - 1][y1 - 1])
+    print(regionsum)
