fd7cd4c7b9d565410401f69d39fe3ccde7c27333
Kim-TaeUk <xodnr0130@naver.com>
2023-10-31 21:44:18 +0900

    곱셈

---
+++ BaekJoon/Math/2588.py
@@ -0,0 +1,9 @@
+import sys
+
+num1 = sys.stdin.readline().rstrip()
+num2 = sys.stdin.readline().rstrip()
+
+print(int(num1) * int(num2[2]))
+print(int(num1) * int(num2[1]))
+print(int(num1) * int(num2[0]))
+print(int(num1) * int(num2))
