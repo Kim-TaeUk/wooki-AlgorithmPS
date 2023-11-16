b7ef9900cbe7095b368e00dc944b0b25daaca4a0
Kim-TaeUk <xodnr0130@naver.com>
2023-11-16 08:11:49 -0500

    인공지능 시계

---
+++ BaekJoon/Math/2530.py
@@ -0,0 +1,22 @@
+import sys
+
+H, M, S = map(int, sys.stdin.readline().split())
+D = int(sys.stdin.readline())
+
+S += D % 60
+D = D // 60
+if S >= 60:
+    S -= 60
+    M += 1
+
+M += D % 60
+D = D // 60
+if M >= 60:
+    M -= 60
+    H += 1
+
+H += D % 24
+if H >= 24:
+    H -= 24
+
+print(H, M, S)
