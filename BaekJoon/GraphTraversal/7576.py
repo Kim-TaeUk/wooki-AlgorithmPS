70e1aaa576ed7057f1bfc800dd68cfb56465dbed
Kim-TaeUk <xodnr0130@naver.com>
2024-02-22 22:26:03 +0900

    토마토
    로직 수정

--- BaekJoon/GraphTraversal/7576.py
+++ BaekJoon/GraphTraversal/7576.py
@@ -3,25 +3,27 @@ import sys
 
 
 def bfs():
-    # 시작점이 될 수 있는 좌표가 모두 queue에 들어가 있는 상태로 bfs 시작!
     while queue:
         current_x, current_y = queue.popleft()
 
         for i in range(4):
             next_x, next_y = current_x + dx[i], current_y + dy[i]
 
-            if 0 <= next_x < N and 0 <= next_y < M and box[next_x][next_y] == 0:
-                box[next_x][next_y] = box[current_x][current_y] + 1
-                queue.append([next_x, next_y])
+            if next_x < 0 or next_x >= N or next_y < 0 or next_y >= M:
+                continue
+            if distance[next_x][next_y] >= 0:
+                continue
+            distance[next_x][next_y] = distance[current_x][current_y] + 1
+            queue.append([next_x, next_y])
 
 
 def find_max():
     has_zero = False
     max_value = float('-inf')
 
-    for row in box:
+    for row in distance:
         for element in row:
-            if element == 0:
+            if element == -1:
                 has_zero = True
                 break
             if element > max_value:
@@ -30,23 +32,22 @@ def find_max():
     if has_zero:
         print(-1)
     else:
-        print(max_value - 1)
+        print(max_value)
 
 
-# 입력
 M, N = map(int, sys.stdin.readline().split())
 box = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-# 필요한 변수 생성
-queue = deque()  # queue랑
-dx = [-1, 0, 1, 0]  # 상하좌우 체크
+queue = deque()
+dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
+distance = [[0 for _ in range(M)] for _ in range(N)]
 
-# 시작점이 여러개이기 때문에, 순회하면서 시작점이 되는 것들을 모두 queue에 넣기!
 for i in range(N):
     for j in range(M):
         if box[i][j] == 1:
             queue.append([i, j])
-
+        if box[i][j] == 0:
+            distance[i][j] = -1
 bfs()
 find_max()
