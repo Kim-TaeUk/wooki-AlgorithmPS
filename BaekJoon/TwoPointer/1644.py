2b2f335f4c62dac7553ae666b611dcf2bfac6bf2
Kim-TaeUk <xodnr0130@naver.com>
2024-02-24 01:27:17 +0900

    소수의 연속합

---
+++ BaekJoon/TwoPointer/1644.py
@@ -0,0 +1,31 @@
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+if N == 1:  # 1일 때 예외처리 안하면 IndexError 발생
+    print(0)
+    exit()
+prime_num = []
+for i in range(2, N + 1):
+    isPrime = True
+    for j in range(2, int(i ** 0.5) + 1):
+        if i % j == 0:
+            isPrime = False
+            break
+    if isPrime:
+        prime_num.append(i)
+
+res = 0
+rt = 0
+sum = prime_num[0]
+
+for lt in range(len(prime_num)):
+    while sum < N and rt < len(prime_num) - 1:
+        rt += 1
+        sum += prime_num[rt]
+    if sum == N:
+        res += 1
+        sum -= prime_num[lt]
+    elif sum > N:
+        sum -= prime_num[lt]
+
+print(res)
