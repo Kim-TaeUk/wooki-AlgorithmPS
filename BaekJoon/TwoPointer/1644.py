d9d18d3e8a91c9be7e35d22e5f9d81d21d8444ba
Kim-TaeUk <xodnr0130@naver.com>
2024-02-25 16:29:01 +0900

    소수의 연속합
    시간 7052ms -> 380ms
    에라토스테네스의 체

--- BaekJoon/TwoPointer/1644.py
+++ BaekJoon/TwoPointer/1644.py
@@ -1,18 +1,26 @@
 import sys
 
+
+# 에라토스테네스의 체 이용
+def get_prime(n):
+    arr = [True for _ in range(n + 1)]
+    arr[0] = False
+    arr[1] = False
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if arr[i]:
+            j = 2
+            while i * j <= n:
+                arr[i * j] = False
+                j += 1
+    return [num for num, is_Prime in enumerate(arr) if is_Prime]
+
+
 N = int(sys.stdin.readline().rstrip())
 if N == 1:  # 1일 때 예외처리 안하면 IndexError 발생
     print(0)
     exit()
-prime_num = []
-for i in range(2, N + 1):
-    isPrime = True
-    for j in range(2, int(i ** 0.5) + 1):
-        if i % j == 0:
-            isPrime = False
-            break
-    if isPrime:
-        prime_num.append(i)
+prime_num = get_prime(N + 1)
 
 res = 0
 rt = 0
