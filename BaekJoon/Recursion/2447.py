58fa21db365ea1dda2343c66f88bd195aa1d7aee
Kim-TaeUk <xodnr0130@naver.com>
2024-03-07 17:23:46 +0900

    TLE 별 찍기 - 10

---
+++ BaekJoon/Recursion/2447.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def draw(i, j, N):
+    if (i // N) % 3 == 1 and (j // N) % 3 == 1:
+        print(' ', end='')
+    else:
+        if N // 3 == 0:
+            print('*', end='')
+        else:
+            draw(i, j, N // 3)
+
+
+N = int(sys.stdin.readline().rstrip())
+for i in range(N):
+    for j in range(N):
+        draw(i, j, N)
+    print('')
