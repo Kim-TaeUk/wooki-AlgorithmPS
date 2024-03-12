53bb76f7675c54a16f51101f8f0e3b255a88abd2
Kim-TaeUk <xodnr0130@naver.com>
2024-03-12 22:16:55 +0900

    N과 M (9)

---
+++ BaekJoon/Backtracking/15663.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def func(K):
+    if K == M:
+        print(" ".join(map(str, arr)))
+        return
+
+    tmp = 0
+    for idx, x in lst:
+        if not is_used[idx] and tmp != x:
+            is_used[idx] = True
+            arr[K] = x
+            tmp = arr[K]
+            func(K + 1)
+            is_used[idx] = False
+
+
+N, M = map(int, sys.stdin.readline().split())
+lst = list(map(int, sys.stdin.readline().split()))
+lst.sort()
+lst = list(enumerate(lst))
+arr = [0] * M
+is_used = [False] * (N + 1)
+func(0)
