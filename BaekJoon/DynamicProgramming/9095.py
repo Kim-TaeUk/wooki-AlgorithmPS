95714a8745ea4e1006c53c2e996475cde0a56f93
Kim-TaeUk <xodnr0130@naver.com>
2024-03-13 17:16:33 +0900

    1, 2, 3 더하기

---
+++ BaekJoon/DynamicProgramming/9095.py
@@ -0,0 +1,10 @@
+import sys
+
+T = int(sys.stdin.readline().rstrip())
+dp = [0] * 20
+dp[1], dp[2], dp[3] = 1, 2, 4
+for i in range(4, 12):
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+for _ in range(T):
+    N = int(sys.stdin.readline().rstrip())
+    print(dp[N])
