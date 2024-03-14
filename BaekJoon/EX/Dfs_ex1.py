5fc3d1b4a9dd1feac05169f78c9638cf69c5132a
Kim-TaeUk <xodnr0130@naver.com>
2024-03-14 17:58:02 +0900

    Traversal in Connected Graph, non-recursive DFS

---
+++ BaekJoon/EX/Dfs_ex1.py
@@ -0,0 +1,40 @@
+"""
+input
+5
+7
+3 1
+2 3
+4 1
+5 2
+5 4
+3 5
+2 4
+"""
+import sys
+
+
+# Traversal in Connected Graph, non-recursive DFS
+def dfs():
+    stack = []
+    stack.append(2)  # start(DFS 시작점)
+    visited[2] = True
+
+    while stack:
+        current = stack.pop()
+        for nxt in adjacent_list[current]:
+            if visited[nxt]:
+                continue
+            stack.append(nxt)
+            visited[nxt] = True
+
+
+V = int(sys.stdin.readline().rstrip())  # vertex (정점)
+E = int(sys.stdin.readline().rstrip())  # edge (간선)
+adjacent_list = [[] for _ in range(V + 1)]
+for _ in range(E):
+    start, end = map(int, sys.stdin.readline().split())
+    adjacent_list[start].append(end)
+    adjacent_list[end].append(start)
+
+visited = [False for _ in range(V + 1)]
+dfs()
