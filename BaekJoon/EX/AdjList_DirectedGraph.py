728105985d1b8fd5088d2a8e436f53fae21c4876
Kim-TaeUk <xodnr0130@naver.com>
2024-03-14 17:14:45 +0900

    Directed Graph and Undirected Graph
    by Adjacency Matrix and Adjacency List

---
+++ BaekJoon/EX/AdjList_DirectedGraph.py
@@ -0,0 +1,20 @@
+"""
+input
+5
+7
+3 1
+2 3
+4 1
+5 2
+5 4
+3 5
+2 4
+"""
+import sys
+
+V = int(sys.stdin.readline().rstrip())  # vertex (정점)
+E = int(sys.stdin.readline().rstrip())  # edge (간선)
+adjacent_list = [[] for _ in range(V + 1)]
+for _ in range(E):
+    start, end = map(int, sys.stdin.readline().split())
+    adjacent_list[start].append(end)
