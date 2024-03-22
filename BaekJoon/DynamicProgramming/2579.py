76d633b2e6b8e4fda9f151951e3e3bcd2277f7c8
Kim-TaeUk <xodnr0130@naver.com>
2024-03-22 18:58:34 +0900

    계단 오르기 - 2차원 배열

--- BaekJoon/DynamicProgramming/2579.py
+++ BaekJoon/DynamicProgramming/2579.py
@@ -5,17 +5,19 @@ scores = [0]
 for _ in range(N):
     scores.append(int(sys.stdin.readline().rstrip()))
 
-if N == 1:
+if N == 1:  # N = 1 예외처리 안해주면 for 문에서 에러 발생
     print(scores[1])
     exit()
 
-dp = [[0, 0, 0] for _ in range(N + 3)]
+dp = [[0 for _ in range(3)] for _ in range(N + 3)]
 dp[1][1] = scores[1]
 dp[1][2] = 0
 dp[2][1] = scores[2]
 dp[2][2] = scores[1] + scores[2]
 
 for i in range(3, N + 1):
+    # dp[i][j]: 현재까지 j개의 계단을 연속해서 밟고 i번째 계단까지 올라섰을 때 점수의 최댓값
+    # (단, i번째 계단은 반드시 밟아야 함)
     dp[i][1] = max(dp[i - 2][1], dp[i - 2][2]) + scores[i]
     dp[i][2] = dp[i - 1][1] + scores[i]
 
