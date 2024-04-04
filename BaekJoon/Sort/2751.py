2d77fe2e9ee0cc32fddbc22ad693944429f59352
Kim-TaeUk <xodnr0130@naver.com>
2024-04-05 03:54:20 +0900

    수 정렬하기 2

--- BaekJoon/Sort/2751.py
+++ BaekJoon/Sort/2751.py
@@ -1,11 +1,5 @@
 import sys
 
-N = int(sys.stdin.readline())
-N_list = []
-for _ in range(N):
-    N_list.append(int(sys.stdin.readline()))
-
-N_list.sort()
-
-for x in N_list:
-    print(x)
+N = int(sys.stdin.readline().rstrip())
+N_list = sorted([int(sys.stdin.readline().rstrip()) for _ in range(N)])
+print(*N_list, sep="\n")
