1c0afc94f14ef17c8c6fff4175fe65f22aff4470
Kim-TaeUk <xodnr0130@naver.com>
2024-04-08 15:38:44 +0900

    통계학

--- BaekJoon/Implementation/2108.py
+++ BaekJoon/Implementation/2108.py
@@ -2,11 +2,7 @@ import sys
 from collections import Counter
 
 N = int(sys.stdin.readline().strip())
-numbers = []
-for _ in range(N):
-    numbers.append(int(sys.stdin.readline().strip()))
-
-numbers.sort()
+numbers = sorted([int(sys.stdin.readline().rstrip()) for _ in range(N)])
 
 if sum(numbers) > 0:
     print(int(sum(numbers) / N + 0.5))
@@ -14,13 +10,14 @@ else:
     print(int(sum(numbers) / N - 0.5))
 
 print(numbers[N // 2])
+
 counts = Counter(numbers)
 max_count = max(counts.values())
 freqvalue = [item for item, count in counts.items() if count == max_count]
+
 if len(freqvalue) > 1:
     print(freqvalue[1])
 else:
     print(freqvalue[0])
-print(max(numbers) - min(numbers))
 
-# sys.stdin.readline().strip() 안쓰면 시간초과
+print(max(numbers) - min(numbers))
