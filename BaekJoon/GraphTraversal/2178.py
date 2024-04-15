68e1d1e6929d0adc55427851e9e43f177ba6c0e0
Kim-TaeUk <xodnr0130@naver.com>
2024-04-15 23:49:09 +0900

    미로 탐색

--- BaekJoon/GraphTraversal/2178.py
+++ BaekJoon/GraphTraversal/2178.py
@@ -1,32 +1,31 @@
 import sys
+from collections import deque
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
 
 def bfs(start_x, start_y, end_x, end_y):
-    # 필요한 변수 생성
-    queue = []  # queue랑
-    dx = [-1, 0, 1, 0]  # 상하좌우 체크
-    dy = [0, 1, 0, -1]
-
-    # queue에 시작 데이터 삽입하여 시작
+    queue = deque()
     queue.append([start_x, start_y])
 
-    # 단위 작업 - 방문 체크하고, 길이 기록(이전 좌표에서 +1)
     visited[start_x][start_y] = True
     distance[start_x][start_y] = 1
 
-    while queue:  # queue에 데이터가 있는 동안
-        current_x, current_y = queue.pop(0)  # queue에서 데이터 빼주고
-        if (current_x, current_y) == (end_x, end_y):  # 도착지 좌표면 종료!
+    while queue:
+        current_x, current_y = queue.popleft()
+        if (current_x, current_y) == (end_x, end_y):
             return distance[current_x][current_y]
 
-        for i in range(4):  # 4방향 처리
+        for i in range(4):
             next_x, next_y = current_x + dx[i], current_y + dy[i]
-
-            if 0 <= next_x < N and 0 <= next_y < M and maze[next_x][next_y] == 1 and not visited[next_x][next_y]:
-                # 단위 작업 - 방문 체크하고, 길이 기록(이전 좌표에서 +1)
+            if next_x < 0 or next_x >= N or next_y < 0 or next_y >= M:
+                continue
+            if visited[next_x][next_y]:
+                continue
+            if maze[next_x][next_y] == 1:
                 visited[next_x][next_y] = True
                 distance[next_x][next_y] = distance[current_x][current_y] + 1
-                # 그리고 queue에 삽입해주기까지
                 queue.append([next_x, next_y])
 
 
