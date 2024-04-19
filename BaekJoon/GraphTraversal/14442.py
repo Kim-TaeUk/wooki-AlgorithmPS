27fee20113b7f1bcd839d85174028b8769ad94d5
Kim-TaeUk <xodnr0130@naver.com>
2024-04-19 18:14:38 +0900

    벽 부수고 이동하기 2

---
+++ BaekJoon/GraphTraversal/14442.py
@@ -0,0 +1,45 @@
+import sys
+from collections import deque
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+
+def bfs():
+    global board, distance
+    queue = deque()
+    queue.append([0, 0, K])
+    distance[0][0][K] = 1
+
+    while queue:
+        current_x, current_y, crashed = queue.popleft()
+        if current_x == N - 1 and current_y == M - 1:
+            return distance[current_x][current_y][crashed]
+
+        for i in range(4):
+            next_x, next_y = current_x + dx[i], current_y + dy[i]
+            if next_x < 0 or next_x >= N or next_y < 0 or next_y >= M:
+                continue
+
+            # next_x, next_y가 벽 & 벽 파괴 가능
+            if board[next_x][next_y] == 1:
+                if distance[next_x][next_y][crashed - 1] != 0:  # 방문한 곳 제외
+                    continue
+                if crashed <= 0:
+                    continue
+                distance[next_x][next_y][crashed - 1] = distance[current_x][current_y][crashed] + 1
+                queue.append([next_x, next_y, crashed - 1])
+
+            # next_x, next_y가 벽X
+            if board[next_x][next_y] == 0:
+                if distance[next_x][next_y][crashed] != 0:  # 방문한 곳 제외
+                    continue
+                distance[next_x][next_y][crashed] = distance[current_x][current_y][crashed] + 1
+                queue.append([next_x, next_y, crashed])
+    return -1
+
+
+N, M, K = map(int, sys.stdin.readline().split())
+board = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(N)]
+distance = [[[0 for _ in range(K + 1)] for _ in range(M)] for _ in range(N)]
+print(bfs())
