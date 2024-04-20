c15a9814e36f2be2a2fbb3243bc693b4cc24140c
Kim-TaeUk <xodnr0130@naver.com>
2024-04-21 00:39:20 +0900

    1, 2, 3 더하기

--- BaekJoon/DynamicProgramming/9095.py
+++ BaekJoon/DynamicProgramming/9095.py
@@ -1,8 +1,11 @@
 import sys
 
 T = int(sys.stdin.readline().rstrip())
-dp = [0] * 20
-dp[1], dp[2], dp[3] = 1, 2, 4
+
+# dp[i] = i를 1, 2, 3의 합으로 나타내는 방법의 수
+dp = [0 for _ in range(20)]  # 점화식
+dp[1], dp[2], dp[3] = 1, 2, 4  # 초기값
+
 for i in range(4, 12):
     dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
 for _ in range(T):
