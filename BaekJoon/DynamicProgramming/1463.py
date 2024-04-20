930f0c54201a7f9a714cc48e54a0b0a49bd42d42
Kim-TaeUk <xodnr0130@naver.com>
2024-04-21 00:42:19 +0900

    1로 만들기

--- BaekJoon/DynamicProgramming/1463.py
+++ BaekJoon/DynamicProgramming/1463.py
@@ -3,10 +3,11 @@ import sys
 N = int(sys.stdin.readline().rstrip())
 
 # dp[i] = i를 1로 만들기 위해 필요한 연산 사용 횟수의 최소값
-dp = [0 for _ in range(N + 1)]  # 점화식
+dp = [0 for _ in range(N + 1)]
 dp[1] = 0  # 초기값
 
 for i in range(2, N + 1):
+    # 점화식
     dp[i] = dp[i - 1] + 1
     if i % 2 == 0:
         dp[i] = min(dp[i], dp[i // 2] + 1)
