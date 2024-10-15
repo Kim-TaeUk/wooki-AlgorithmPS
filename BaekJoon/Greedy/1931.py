7faa6d5043fcf4a10b67e2c1a353487487cef075
Kim-TaeUk <xodnr0130@naver.com>
2024-10-16 01:46:05 +0900

    회의실 배정
    그리디

--- BaekJoon/Greedy/1931.py
+++ BaekJoon/Greedy/1931.py
@@ -1,18 +1,17 @@
-n = int(input())
-n_list = [list(map(int, input().split())) for _ in range(n)]
+import sys
 
-n_list.sort(key=lambda x: (x[1], x[0]))
+N = int(sys.stdin.readline().rstrip())
+lst = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+lst.sort(key=lambda x: (x[1], x[0]))
 
-cnt = 0
-cur = 0
+cnt, cur_time = 0, 0
 
-for i in range(n):
-    if cur <= n_list[i][0]:
-        cur = n_list[i][1]
-        cnt += 1
+for x in lst:
+    if cur_time > x[0]:
+        continue
+    cur_time = x[1]
+    cnt += 1
 
 print(cnt)
 
-# 회의의 시작 시간 = 종료 시간일 때 고려하지 않은 풀이여서 틀렸었음
-# cnt = 0, cur = 0으로 바꿔주거나
-# cnt = 1, cur = n_list[0][1] 그대로에 for문을 range(1, n)으로 주면 됨
+# 회의의 시작 시간 = 종료 시간일 때 고려하지 않는 것 유의
