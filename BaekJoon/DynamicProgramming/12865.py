34a7019564244d97f3432caf4a58a1d395475ee2
Kim-TaeUk <xodnr0130@naver.com>
2024-10-18 02:47:50 +0900

    평범한 배낭
    0-1 knapsack
    Time: O(NK), Space: O(NK)

--- BaekJoon/DynamicProgramming/12865.py
+++ BaekJoon/DynamicProgramming/12865.py
@@ -5,9 +5,10 @@ bag = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 dp = [[0 for _ in range(K + 1)] for _ in range(N + 1)]
 
 for i in range(1, N + 1):
+    weight, value = bag[i - 1]
     for j in range(1, K + 1):
-        if j >= bag[i - 1][0]:
-            dp[i][j] = max(bag[i - 1][1] + dp[i - 1][j - bag[i - 1][0]], dp[i - 1][j])
+        if j >= weight:
+            dp[i][j] = max(value + dp[i - 1][j - weight], dp[i - 1][j])
         else:
             dp[i][j] = dp[i - 1][j]
 
